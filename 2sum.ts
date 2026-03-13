64761a61c2caf7d63667ecbcf8a7bad6f8ef4060
Prateek <prateek45698@gmail.com>
2026-03-13 10:11:07 +0530

    refactor: improve twoSum function implementation

--- 2sum.ts
+++ 2sum.ts
@@ -8,3 +8,21 @@ function twoSumBrute(nums: number[], target: number): number[] {
     }
     return []
 }
+
+function twoSum(nums: number[], target: number): number[] {
+    const map = new Map<number, number>() // value -> index
+
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i]
+
+        // check if complement already exists
+        if (map.has(complement)) {
+            return [map.get(complement)!, i]
+        }
+
+        // store current number with its index
+        map.set(nums[i], i)
+    }
+
+    return []
+}
